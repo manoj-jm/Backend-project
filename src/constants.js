298a397810f157d718ff3e9ce4f60054fce3164e
manoj <jmanojjmvr49@gmail.com>
2025-02-26 12:43:43 +0530

    "connect to mongo atlas"

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// why we need constants.js file ? - In backend development, a constants.js file is typically used to store fixed values that don’t change throughout the application. This improves maintainability, reusability, and clarity. 
+
+export const DB_NAME = 'mydb';
---
+++ src/index.js
@@ -0,0 +1,10 @@
+// require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/index.js";
+
+// dotenv.config({
+//   path: "../.env",
+// });
+
+connectDB();
