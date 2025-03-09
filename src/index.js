c8aec12a44475f7e17cc4bb6cd9fe0888710ff5a
manoj <jmanojjmvr49@gmail.com>
2025-03-09 21:55:11 +0530

    dot env configured with path

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
-dotenv.config();
 // import express from "express";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+dotenv.config({
+  path: "./.env",
+});
 connectDB()
   .then(() => {
     try {
@@ -16,9 +18,6 @@ connectDB()
     } catch (error) {
       console.log(`Server is not running due to ${error}`);
     }
-
-    
-    
   })
   .catch((err) => {
     console.log(" MONGO DB CONNECTION FAILED", err);
