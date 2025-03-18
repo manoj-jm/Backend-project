d9b7bfe281c742ba3e20f4f94f004e7ec588b160
manoj <jmanojjmvr49@gmail.com>
2025-03-18 19:56:59 +0530

    feat: enhance user routes with secure actions for password change, account updates, and profile retrieval

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,23 @@
 import { Router } from "express";
 import {
+  registerUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
-  registerUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  getUserChannelProfile,
+  updateUserCoverImg,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 const router = Router();
 
+// Registration (Handles both avatar & cover image upload)
 router.route("/register").post(
   upload.fields([
     { name: "avatar", maxCount: 1 },
@@ -17,13 +26,20 @@ router.route("/register").post(
   registerUser
 );
 
+// Authentication Routes
 router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser); // its good to use get 
+router.route("/refresh-token").post(refreshAccessToken);
 
-// secure routes
-router.route("/logout").post(verifyJWT, logoutUser);
+// Secure User Actions
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-user-details").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/update-coverimage").patch(verifyJWT, upload.single("coverimage"), updateUserCoverImg);
 
-// router.route("/your-route")
-//   .get(middleware1, middleware2, controllerFunction)  // Handle GET request
+// Profile & Watch History
+router.route("/profile/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 
-router.route("/refresh-token").post(refreshAccessToken)
 export default router;
